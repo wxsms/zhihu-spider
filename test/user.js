4929673fada4c2a2b7eb4d009a97cc71abc769ea
wxsm <edisond@qq.com>
2016-09-07 21:57:13 +0800

    add test

--- test/user.js
+++ test/user.js
@@ -8,20 +8,40 @@ const jsdom = require('jsdom');
 const userProfile = fs.readFileSync(path.join(__dirname, '/data/userProfile.txt')).toString('utf8');
 
 describe('Parsers', function () {
-    describe('user parser', function () {
-        it('should parse hashId correctly', function (done) {
-            const hashIdParser = require('./../src/parsers/user/hashIdParser');
-            jsdom.env(userProfile, (err, window) => {
-                if (err) {
-                    window.close();
-                    done(err);
-                } else {
-                    const $ = require('jquery')(window);
-                    let item = hashIdParser($);
-                    assert.equal(item.hashId, '6a246a16b892bf7cbbb62ae586190c4a');
-                    done();
-                }
-            });
-        });
+  describe('user parser', function () {
+    it('should parse hashId correctly', function (done) {
+      const hashIdParser = require('./../src/parsers/user/hashIdParser');
+      jsdom.env(userProfile, (err, window) => {
+        if (err) {
+          window.close();
+          done(err);
+        } else {
+          const $ = require('jquery')(window);
+          let item = hashIdParser($);
+          assert.equal(item.hashId, '6a246a16b892bf7cbbb62ae586190c4a');
+          done();
+        }
+      });
     });
+
+    it('should parse basic info correctly', function (done) {
+      const hashIdParser = require('./../src/parsers/user/basicParser');
+      jsdom.env(userProfile, (err, window) => {
+        if (err) {
+          window.close();
+          done(err);
+        } else {
+          const $ = require('jquery')(window);
+          let item = hashIdParser($);
+          assert.deepEqual(item, {
+            bio: "Web程序员，接私活",
+            description: "http://wxsm.space",
+            gender: "male",
+            name: "wxsm"
+          });
+          done();
+        }
+      });
+    });
+  });
 });
