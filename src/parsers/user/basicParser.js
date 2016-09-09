d81ef60d25dcd304d7330d95a2f7c55f7fedbd43
wxsm <edisond@qq.com>
2016-09-09 10:38:58 +0800

    fix some parser bug

--- src/parsers/user/basicParser.js
+++ src/parsers/user/basicParser.js
@@ -6,6 +6,7 @@ module.exports = function ($) {
     let profileCard = $('.ProfileCard');
     user.name = profileCard.find('.name').text();
     user.bio = profileCard.find('.bio').text();
+    user.gender = '';
     let genderDiv = profileCard.find('.gender');
     if (genderDiv.html() && genderDiv.html().indexOf) {
       user.gender = genderDiv.html().indexOf('female') >= 0 ? 'female' : 'male';
