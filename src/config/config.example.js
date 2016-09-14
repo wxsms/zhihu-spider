aa23ba4793779c3fd2551cb410cc780daf1812f3
wxsm <edisond@qq.com>
2016-09-14 11:44:47 +0800

    fix session timeout problem

--- src/config/config.example.js
+++ src/config/config.example.js
@@ -3,8 +3,10 @@
 //rename this file to zhihu.js and fill with your own value
 module.exports = {
   user: {
-    phone_num: 'xxx',
-    password: 'xxx'
+    phone_num: 'xxx',//the login id
+    password: 'xxx',//the login password
+    name: 'wxsm', //the use url pattern to fetch (can be not the login user)
+    remember_me: true //use true to avoid session timeout
   },
   userSpider: {
     seed: 'wxsm'
